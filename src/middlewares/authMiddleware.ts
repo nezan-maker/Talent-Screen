04a7656b22e20c991f9b426d1ef022213e7f564d
nezan-maker <nezaniel2@gmail.com>
2026-04-10 22:49:36 +0200

    Added authorization middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,6 +2,7 @@ import type { Response, NextFunction } from "express";
 import type { I_Request } from "../controllers/authControl.js";
 import env from "../config/env.js";
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 const ACCESS_SECRET = env.ACCESS_SECRET;
 const middleAuth = async (
   req: I_Request,
@@ -19,4 +20,17 @@ const middleAuth = async (
     return res.status(500).json({ error: "Internal server error" });
   }
   const user_details = jwt.verify(identity_cookie, ACCESS_SECRET);
+  if (typeof user_details !== "object") {
+    return;
+  }
+  const user_email = user_details.email;
+  const user = await User.findOne({ user_email });
+  if (!user) {
+    return res.status(404).json({
+      data_error: "User could not be found.Token corrupted or expired",
+    });
+  }
+  if (user) {
+    next();
+  }
 };

6233d27620193047376ab6e055eab9063e82f327
nezan-maker <nezaniel2@gmail.com>
2026-04-21 19:40:20 +0200

    Refined applicants registeration by plucking out the unnecssary that are taken care of by resume

--- src/controllers/applicantControl.ts
+++ src/controllers/applicantControl.ts
@@ -2,18 +2,9 @@ import type { Response, Request } from "express";
 import * as excel from "exceljs";
 import Applicant from "../models/Applicant.js";
 import { Readable } from "stream";
-interface ApplicantData {
+interface Applicant_ {
   applicant_name: string;
   job_title: string;
-  skills: string | string[];
-  education_certificates: string | string[];
-  additional_info: string | string[];
-  experience_in_years: number;
-}
-interface Applicant extends ApplicantData {
-  skills: string[];
-  education_certificates: string[];
-  additional_info: string[];
 }
 const applicantControl = async (req: Request, res: Response) => {
   if (!req.file) {
@@ -24,23 +15,11 @@ const applicantControl = async (req: Request, res: Response) => {
     const { raw_application_data }: { raw_application_data: string } = req.body;
     const application_data = JSON.parse(raw_application_data);
     for (let i = 0; i < application_data.length; i++) {
-      const current_json: ApplicantData = application_data[i];
-      if (
-        typeof current_json.skills !== "string" ||
-        typeof current_json.additional_info !== "string" ||
-        typeof current_json.education_certificates !== "string"
-      )
-        return;
-      let skills = current_json.skills.split(",");
-      let additional_info = current_json.skills.split(",");
-      let education_certificates = current_json.skills.split(",");
-      let applicant_json: Applicant = {
+      const current_json: Applicant_ = application_data[i];
+
+      let applicant_json: Applicant_ = {
         applicant_name: current_json.applicant_name,
         job_title: current_json.job_title,
-        skills,
-        additional_info,
-        education_certificates,
-        experience_in_years: current_json.experience_in_years,
       };
       const oldApplicant = await Applicant.findOne({
         applicant_name: current_json.applicant_name,
@@ -117,27 +96,14 @@ const applicantControl = async (req: Request, res: Response) => {
     });
   });
   for (let i = 0; i < file_content_json.length; i++) {
-    const current_json: ApplicantData = file_content_json[i];
+    const current_json: Applicant_ = file_content_json[i];
     if (!current_json)
       return res
         .status(400)
         .json({ data_error: "Spreadsheet does not match required structure" });
-    if (
-      typeof current_json.skills !== "string" ||
-      typeof current_json.additional_info !== "string" ||
-      typeof current_json.education_certificates !== "string"
-    )
-      return;
-    let skills = current_json.skills.split(",");
-    let additional_info = current_json.skills.split(",");
-    let education_certificates = current_json.skills.split(",");
-    let applicant_json: Applicant = {
+    let applicant_json: Applicant_ = {
       applicant_name: current_json.applicant_name,
       job_title: current_json.job_title,
-      skills,
-      additional_info,
-      education_certificates,
-      experience_in_years: current_json.experience_in_years,
     };
     const oldApplicant = await Applicant.findOne({
       applicant_name: current_json.applicant_name,

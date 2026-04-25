4e5d7f4d64761ef17dc1bf8d6164fc46e32a9608
nezan-maker <nezaniel2@gmail.com>
2026-04-25 18:49:57 +0200

    changed port to 10000

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ const __dirname = dirname(__filename);
 const app = express();
 let PORT: number;
 if (env.PORT) {
-  PORT = parseInt(env.PORT || "5000", 10);
+  PORT = parseInt(env.PORT || "10000", 10);
 } else {
   throw new Error("Could not load environment variables");
 }
